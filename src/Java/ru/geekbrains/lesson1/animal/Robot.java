d2561bc6cf9f9160805cc60a70167f237810c827
amzhulanov <amzhulanov@ya.ru>
2019-03-27 21:29:02 +0700

    Добавлена обработка препятствий. Переопределён метод toString() для вывода информации о команде

--- src/Java/ru/geekbrains/lesson1/animal/Robot.java
+++ src/Java/ru/geekbrains/lesson1/animal/Robot.java
@@ -2,6 +2,8 @@ package Java.ru.geekbrains.lesson1.animal;
 
 import Java.ru.geekbrains.lesson1.Participant;
 
+import static java.lang.System.*;
+
 /**
  * Робот не является животным, но может учавствовать в соревнованиях
  * так как реализует интерфейс {@link Participant}
@@ -9,6 +11,9 @@ import Java.ru.geekbrains.lesson1.Participant;
 public class Robot implements Participant {
 
     private String name;
+    private boolean isOnDistance;
+    private int runDistance;
+    private int jumpHeight;
 
     public Robot(String name) {
         this.name = name;
@@ -16,21 +21,53 @@ public class Robot implements Participant {
 
     @Override
     public boolean isOnDistance() {
-        return false; // TODO доработать по аналогии с классами животных
+        return isOnDistance;
     }
 
     @Override
     public void run(int distance) {
-        // TODO доработать по аналогии с классами животных
+        if (!isOnDistance) {
+            return;
+        }
+        if (distance > runDistance) {
+            isOnDistance = false;
+            System.out.println(String.format("Робот %s не смог  пробежать дистанцию %d и он снимается с соревнований. На прошлых соревнованиях он достиг отметки %d", name, distance, runDistance));
+            }
+            return;
+
+        //Если последний символ из имени животного гласный, то женский род.
+
+        //System.out.println(String.format("Робот %s смог пробежать дистанцию %d", name, distance));
+       // System.out.println(String.format("Робот %s смог пробежать дистанцию %d ", name, distance));
     }
 
     @Override
     public void jump(int height) {
-        // TODO доработать по аналогии с классами животных
+        if (!isOnDistance) {
+            return;
+        }
+        if (height > jumpHeight) {
+            isOnDistance = false;
+            System.out.println(String.format("Робот %s не смог взять высоту %d и он снимается с соревнований" , name, height));
+
+            return;
+        }
+        System.out.println(String.format("Робот %s прыгнул на высоту %d. Её личный рекорд %d", name, height));
     }
 
     @Override
     public void swim(int distance) {
-        // TODO доработать по аналогии с классами животных
+        isOnDistance = false;
+        System.out.println("Робот был тяжёлый и утонул. Их осталось трое.");
+    }
+
+    @Override
+    public String toString() {
+        return "Robot{" +
+                "name='" + name + '\'' +
+                ", isOnDistance=" + isOnDistance +
+                ", runDistance=" + runDistance +
+                ", jumpHeight=" + jumpHeight +
+                '}';
     }
 }
