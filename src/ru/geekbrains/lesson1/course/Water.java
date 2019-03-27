c6209bf2dd616553d86b7b5dc0c49d9fcb138216
amzhulanov <amzhulanov@ya.ru>
2019-03-27 14:03:21 +0700

    Доработан класс Water

--- src/ru/geekbrains/lesson1/course/Water.java
+++ src/ru/geekbrains/lesson1/course/Water.java
@@ -3,13 +3,13 @@ package ru.geekbrains.lesson1.course;
 import ru.geekbrains.lesson1.Participant;
 
 public class Water extends Obstacle {
+    private int distance;
 
-    public Water(int distance) {
-        // TODO доработать по аналогии с другими препятствиями
+    public Water(int distance) { this.distance=100;
     }
 
     @Override
     public void doIt(Participant participant) {
-        // TODO доработать по аналогии с другими препятствиями
+        participant.swim(this.distance);
     }
 }
