daee1ff8c774b2353d9460de084a51a407d03ef9
amzhulanov <amzhulanov@ya.ru>
2019-03-27 22:34:01 +0700

    Отредактирован список участников команды. Добавлен вызов методов ShowTeam, ShowResult

--- src/Java/ru/geekbrains/lesson1/Application.java
+++ src/Java/ru/geekbrains/lesson1/Application.java
@@ -2,6 +2,8 @@ package Java.ru.geekbrains.lesson1;
 
 import Java.ru.geekbrains.lesson1.animal.Cat;
 import Java.ru.geekbrains.lesson1.animal.Dog;
+import Java.ru.geekbrains.lesson1.animal.Human;
+import Java.ru.geekbrains.lesson1.animal.Robot;
 import Java.ru.geekbrains.lesson1.course.Course;
 import Java.ru.geekbrains.lesson1.course.Cross;
 import Java.ru.geekbrains.lesson1.course.Wall;
@@ -16,21 +18,23 @@ public class Application {
     public static void main(String[] args) {
         Team team = new Team(
                 new Cat("Барсик", Color.BLACK, 1, 100, 5),
-                new Cat("Мурка", Color.BLACK, 1, 80, 12),
-                new Dog("Дружок", Color.BLACK,5,150,15)
-
-
-                // TODO здесь должны быть участники всех видов (Cat, Dog, Human, Robot)
+                new Dog("Дружок", Color.BLACK,5,150,15),
+                new Human("Василий",Color.BLACK,28),
+                new Robot("Android")
         );
 
         Course course = new Course(
                 new Cross(50),
                 new Wall(10),
-                new Water(5),
-                new Voice()
+                new Water(5)
+
 
         );
-
+        team.showTeam();
         course.doIt(team);
+        team.showResult();
+
+
     }
+
 }
